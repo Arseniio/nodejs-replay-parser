00eb532cece4640a065fa13064f76ebc49f80a18
tanamotokazuma <pokemonson8897@gmail.com>
2023-01-22 15:03:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 var fs = require('fs');
 var lzma = require('lzma-native');
-const { toNamespacedPath } = require('path');
 var GetMapPoints = require('./MapParser.js')
-
-// function count(mappoints = [],actions = [[],[],[]]){
-//     var total = 0
-//     for(var i = 0;i<actions.length;i++){
-//         total += actions[i][?]
-//     }
-//     var avg = total / actions.length
-//     for(var i = 0;i<actions.length;i++){
-//         variance += (actions[i][?] / avg) ** 2
-//     }
-//     console.log("Variance: ",variance)
-//     return variance
-// }
 
 const readuleb = (input, length) => {
     let result = 0;
     let shift = 0;
     const byte = length
     while (true) {
-        // const byte = input.shift();
         result |= (byte & 0x7f) << shift;
         shift += 7;
         if ((0x80 & byte) === 0) {
@@ -32,8 +17,8 @@ const readuleb = (input, length) => {
         }
     }
 };
-// var file = "./arsenii0 - Toto - Africa (MADD Frenchcore Bootleg) [Hurry boy, it's waiting there for you] (2022-02-22) Osu.osr"
-var file = "./Tanamoto - Apocalyptica - Hall of the Mountain King [Extra] (2020-04-03) Osu-2.osr"
+var file = "[DJG] Kazinak_XXL - Dream Theater - Moment of Betrayal [Awakened Noise Machine] (2021-10-23) Osu.osr"
+//var file = "./Tanamoto - Morimori Atsushi - Time Machine [1] (2022-04-16) Osu-1.osr"
 
 //Has three parts; a single byte which will be either 0x00, indicating that the next two parts are not present,
 //or 0x0b (decimal 11), indicating that the next two parts are present.
@@ -75,9 +60,9 @@ function readByte(len, fileconst, movepointer = true, Rstring = false) {
         movepointer == true ? pos = pos + len : ""
     }
     // console.log('RB:', ret)
-    if (!Rstring){
-        if(len <= 2){
-            if(len == 0) return;
+    if (!Rstring) {
+        if (len <= 2) {
+            if (len == 0) return;
             return Buffer.from(ret).readUInt16LE();
         } else return Buffer.from(ret).readUInt32LE();
     }
@@ -106,12 +91,12 @@ CompressedSize = readByte(4, fileconst)
 compressed_data = []
 for (var i = 0; i < CompressedSize; i++) {
     // console.log(pos)
-    compressed_data.push(readByte(1, fileconst,true,true))
+    compressed_data.push(readByte(1, fileconst, true, true))
 }
 OnlineScoreID = readByte(8, fileconst)
 
-console.log("mode: ",mode)
-console.log("version: ",version)
+console.log("mode: ", mode)
+console.log("version: ", version)
 console.log("md5map: ", md5map)
 console.log("Tanamoto: ", plrname)
 console.log("кто сюда смотрит: ", md5replay)
@@ -129,47 +114,73 @@ console.log("Lifebar: ", Lifebar)
 console.log("TimeStamp: ", TimeStamp)
 console.log("CompressedSize: ", CompressedSize)
 
-lzma.LZMA().decompress(compressed_data, (result) => {
-        let str = '';
-        for( let i = 0; i < result.length; i++)
-        str += (String.fromCharCode(result[i]));
-        fs.writeFileSync('govno.txt',str)
-        // console.log(str.split(','));
-        frames = str.split(',');
-        absTime = 0;
-        actionframes = []
-        for(i = 0; i < frames.length; i++){
-            value = frames[i].split('|');
-            // absTime += Math.abs(parseInt(value[0]));
-            if (value[0] < 0) continue;
-            if (value[1] == 256 && value[2] == -500 && i < 2) continue;
-            absTime += parseInt(value[0]);
-            actionframes[i] = [parseInt(value[0]), parseInt(absTime),parseInt(value[1]),parseInt(value[2]),parseInt(value[3])]
-        }
-        console.log(actionframes)
+lzma.LZMA().decompress(compressed_data, async (result) => {
+    let str = '';
+    for (let i = 0; i < result.length; i++) str += (String.fromCharCode(result[i]));
 
-        // for (i = 0; i < actionframes.length; i++){
-        //     if (actionframes[i][4] == 5 || actionframes[i][4] == 10 || actionframes[i][4] == 1 || actionframes[i][4] == 8) console.log(actionframes[i]);
-        // }
-        str = "";
-        ii = 0;
-        b = 0;
-        MapPoints = GetMapPoints(fs.readFileSync('Apocalyptica - Hall of the Mountain King (pishifat) [Extra].osu',"utf-8"));
+    frames = str.split(',');
+    absTime = 0;
+    actionframes = []
+    for (i = 1; i < frames.length; i++) {
+        value = frames[i].split('|');
+        prevval = value;
+        absTime += parseInt(value[0]);
+        actionframes[i] = [parseInt(value[0]), parseInt(absTime), parseInt(value[1]), parseInt(value[2]), parseInt(value[3])]
+    }
+    goodframes = []
 
-        for(i = 0; i < frames.length; i++){
-            if(actionframes[i] == undefined) continue;
-            str += actionframes[i].toString() + "\n";
-            if ((actionframes[i][4] == 5 || actionframes[i][4] == 10 || actionframes[i][4] == 1 || actionframes[i][4] == 8) && Math.abs(MapPoints[b] - actionframes[i][1])<119.5){
-                console.log(actionframes[i][1],MapPoints[b],Math.abs(MapPoints[b] - actionframes[i][1]));
-                b++;
-            }
-            
-            if(i == frames.length - 1) console.log(actionframes[i]);
+    MapPoints = GetMapPoints(fs.readFileSync('Dream Theater - Moment of Betrayal (Zetera) [Awakened Noise Machine].osu', "utf-8"));
+
+
+    od = 7.5;
+    MaxHitError = 200 - 20 * od;
+
+    Point = 0;
+    avghiterror = 0;
+    hiterrors = [];
+
+    for (let i = 1; i < actionframes.length; i++) {
+        frame = actionframes[i];
+        if (Point == MapPoints.length) break;
+        // if (i > 1) if (frame[4] == actionframes[i-1][4]) continue; 
+        if (frame[1] > MapPoints[Point][2] - MaxHitError && frame[1] < MapPoints[Point][2] + MaxHitError && frame[4] != 0) {
+            goodframes.push(frame);
+            avghiterror += (frame[1] - MapPoints[Point][2])
+            hiterrors.push(frame[1] - MapPoints[Point][2]);
+            Point++;
         }
+    }
+
+    // console.log(goodframes)
+
+    sum = 0;    // ВОТ ОН САМ УР (СЧИТАЮ КАК МУДАК)
+    avghiterror /= hiterrors.length;
+
+    hiterrors.forEach(hiterror => {
+        sum += (hiterror - avghiterror) * (hiterror - avghiterror);
+    });
+    
+    UR = Math.sqrt(sum * (1 / (hiterrors.length - 1))) * 10;
+
+    countp = 0; // сюда можно не смотреть, это чисто я считаю хитеррор
+    countm = 0;
+    divp = 0;
+    divm = 0;
+
+    for (i = 0; i < hiterrors.length; i++) {
+        if (hiterrors[i] < 0) {
+            divm += hiterrors[i]; countm++;
+        }
+        if (hiterrors[i] > 0) {
+            divp += hiterrors[i]; countp++;
+        }
+    }
+
+    divp /= countp;
+    divm /= countm;
+
+    console.log(`Error: ${divm} - ${divp}`)
+
 })
 
 console.log("OnlineScoreID: ", OnlineScoreID)
-
-
-
-// console.log(GetMapPoints(fs.readFileSync('Apocalyptica - Hall of the Mountain King (pishifat) [Extra].osu',"utf-8")))
