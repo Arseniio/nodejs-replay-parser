f4afba81cf942a7709d5d1659c162841484f553b
Arseniio <42149853+Arseniio@users.noreply.github.com>
2023-11-27 23:27:45 +0500

    first working version????
    not really,huge ur diff from game(+-10ms)

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 var fs = require('fs');
 var lzma = require('lzma-native');
 var GetMapPoints = require('./MapParser.js')
+// const simplestatistics = require('simple-statistics');
 
-const readuleb = (input, length) => {
+class HitObj {
+    constructor(tick, x, y) {
+        this.tick = tick;
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
+const readuleb = () => {
     let result = 0;
     let shift = 0;
-    const byte = length
     while (true) {
+        const byte = readByte(1, fileconst)
         result |= (byte & 0x7f) << shift;
-        shift += 7;
         if ((0x80 & byte) === 0) {
-            if (shift < 32 && (byte & 0x40) !== 0) {
-                return result | (~0 << shift);
-            }
-            return result;
+            break;
         }
+        shift += 7;
     }
+    return result;
 };
-var file = "[DJG] Kazinak_XXL - Dream Theater - Moment of Betrayal [Awakened Noise Machine] (2021-10-23) Osu.osr"
+// const readuleb = (input, length) => {
+//     let result = 0;
+//     let shift = 0;
+//     const byte = length
+//     while (true) {
+//         result |= (byte & 0x7f) << shift;
+//         shift += 7;
+//         if ((0x80 & byte) === 0) {
+//             if (shift < 32 && (byte & 0x40) !== 0) {
+//                 return result | (~0 << shift);
+//             }
+//             return result;
+//         }
+//     }
+// };
 //var file = "./Tanamoto - Morimori Atsushi - Time Machine [1] (2022-04-16) Osu-1.osr"
 
 //Has three parts; a single byte which will be either 0x00, indicating that the next two parts are not present,
@@ -30,16 +52,37 @@ function readString(fileconst) {
     firstbyte = readByte(1, fileconst)
     if (firstbyte == 0x00) return
     else if (firstbyte == 0x0b) {
-        uleblen = readByte(1, fileconst)
-        ulebcontext = readByte(uleblen, fileconst, false)
-        len = readuleb(ulebcontext, uleblen)
+        strlen = readuleb()
         retString = []
-        for (var i = 0; i < len; i++) {
+        for (var i = 0; i < strlen; i++) {
             retString.push(readByte(1, fileconst))
         }
         return Buffer.from(retString, 'utf-8').toString();
 
     }
+}
+
+function isPressed(currframe, prevframe) {
+    if ((currframe[4] != prevframe[4]) && (currframe[4] > prevframe[4])) return 1;
+    return 0;
+}
+
+function findNextHitFrame(frames, fromFrame) {
+    for (let i = fromFrame + 1; i < frames.length; i++) {
+        if (((frames[i][4] != frames[i - 1][4]) && (frames[i][4] > frames[i - 1][4]))) {
+            return i;
+        }
+    }
+    return;
+}
+
+function findPrevHitFrame(frames, fromFrame) {
+    for (let i = fromFrame - 1; i > 0; i--) {
+        if (((frames[i - 1][4] != frames[i][4]) && (frames[i - 1][4] < frames[i][4]))) {
+            return i;
+        }
+    }
+    return;
 }
 
 pos = 0
@@ -68,6 +111,17 @@ function readByte(len, fileconst, movepointer = true, Rstring = false) {
     }
 }
 
+// var file = "arsenii0 - pudge - idi nahuj [IDI NAHYI] (2023-08-14) Osu.osr"
+// var file = "menR2.osr"
+// var file = "mendesrepl.osr"
+// var file = "replay-mania_3469849_531873801.osr"
+// var file = "arsenii0 - utsuP - MiKUSABBATH [Collab] (2023-08-21) Osu.osr"
+// var file = "arsenii0 - utsuP - MiKUSABBATH [Collab] (2023-08-21) Osu-1.osr"
+// var file = "arsenii0 - utsuP - MiKUSABBATH [Collab] (2023-08-21) Osu-2.osr"
+// var file = "sq.osr"
+// var file = "replay-osu_726336_3331176568.osr"
+var file = "dt.osr"
+// var file = "asd.osr"
 fileconst = fs.readFileSync(file)
 mode = readByte(1, fileconst)
 version = readByte(4, fileconst)
@@ -103,6 +157,7 @@ console.log("кто сюда смотрит: ", md5replay)
 console.log("R300: ", R300)
 console.log("R100: ", R100)
 console.log("R50: ", R50)
+console.log("Total hits: ", R300 + R100 + R50)
 console.log("RGeNAn: ", RGeki)
 console.log("RKan: ", RKatu)
 console.log("Misis: ", Miss)
@@ -114,6 +169,24 @@ console.log("Lifebar: ", Lifebar)
 console.log("TimeStamp: ", TimeStamp)
 console.log("CompressedSize: ", CompressedSize)
 
+function hasAnyNewInput(currInputs, prevInputs) {
+    return (currInputs & ~prevInputs) !== 0;
+}
+
+function isNewKeyPressed(curr, prev) {
+    return hasAnyNewInput(curr, prev) && curr !== 0;
+}
+
+function isOnCircle(obj, frame, r) {
+    return Math.pow(parseInt(obj[1]) - frame[2], 2) +
+        Math.pow(parseInt(obj[2]) - frame[3], 2) <= Math.pow(r, 2);
+}
+
+function isInHitWindow(obj, frame, border) {
+    return (parseInt(obj[0]) - border) <= frame[1] &&
+        frame[1] <= (parseInt(obj[0]) + border);
+}
+
 lzma.LZMA().decompress(compressed_data, async (result) => {
     let str = '';
     for (let i = 0; i < result.length; i++) str += (String.fromCharCode(result[i]));
@@ -121,66 +194,84 @@ lzma.LZMA().decompress(compressed_data, async (result) => {
     frames = str.split(',');
     absTime = 0;
     actionframes = []
-    for (i = 1; i < frames.length; i++) {
+    //delta,abs,x,y,keys?
+    for (i = 0; i < frames.length; i++) {
         value = frames[i].split('|');
-        prevval = value;
+        if (parseInt(value[0]) == 0 || parseInt(value[0]) == -12345) continue;
         absTime += parseInt(value[0]);
-        actionframes[i] = [parseInt(value[0]), parseInt(absTime), parseInt(value[1]), parseInt(value[2]), parseInt(value[3])]
+        actionframes.push([parseInt(value[0]), parseInt(absTime), parseInt(value[1]), parseInt(value[2]), parseInt(value[3])])
     }
-    goodframes = []
-
-    MapPoints = GetMapPoints(fs.readFileSync('Dream Theater - Moment of Betrayal (Zetera) [Awakened Noise Machine].osu', "utf-8"));
 
 
-    od = 7.5;
-    MaxHitError = 200 - 20 * od;
 
-    Point = 0;
-    avghiterror = 0;
-    hiterrors = [];
+    var obj = GetMapPoints('./dtm.osu');
+    var previnput = 0;
 
-    for (let i = 1; i < actionframes.length; i++) {
-        frame = actionframes[i];
-        if (Point == MapPoints.length) break;
-        // if (i > 1) if (frame[4] == actionframes[i-1][4]) continue; 
-        if (frame[1] > MapPoints[Point][2] - MaxHitError && frame[1] < MapPoints[Point][2] + MaxHitError && frame[4] != 0) {
-            goodframes.push(frame);
-            avghiterror += (frame[1] - MapPoints[Point][2])
-            hiterrors.push(frame[1] - MapPoints[Point][2]);
-            Point++;
+    var negHitError = [];
+    var posHitError = [];
+    var allHitError = [];
+
+    var cssize = 54.4 - 4.48 * cs;
+    var hit50window = 200 - 10 * obj[4];
+    for (var objIndex = 0, frameIndex = 0; objIndex < obj[0].length && frameIndex < frames.length;) {
+        var currObj = [obj[0][objIndex], obj[1][objIndex], obj[2][objIndex]]
+        var currFrame = actionframes[frameIndex]
+        if (currFrame[1] > parseInt(currObj[0]) + hit50window) {
+            previnput = currFrame[4];
+            objIndex++;
+            continue;
         }
+        // console.log(isOnCircle(currObj, currFrame, cssize));
+        if (isNewKeyPressed(parseInt(currFrame[4]), previnput) &&
+            isOnCircle(currObj, currFrame, cssize) &&
+            isInHitWindow(currObj, currFrame, hit50window)
+        ) {
+            hiterror = 0
+            var hiterror = parseInt(currFrame[1]) - parseInt(currObj[0]);
+            if (hiterror < 0)
+                negHitError.push(hiterror);
+            else posHitError.push(hiterror);
+            allHitError.push(hiterror);
+            // if(objIndex<200)
+            console.log(hiterror);
+            objIndex++;
+        }
+        previnput = currFrame[4];
+        frameIndex++;
+        // console.log('frameIndex,objIndex :>> ', frameIndex,objIndex);
+        // clg
     }
 
-    // console.log(goodframes)
+    let negSum = 0, posSum = 0, allSum = 0;
 
-    sum = 0;    // ВОТ ОН САМ УР (СЧИТАЮ КАК МУДАК)
-    avghiterror /= hiterrors.length;
-
-    hiterrors.forEach(hiterror => {
-        sum += (hiterror - avghiterror) * (hiterror - avghiterror);
-    });
+    for (const err of negHitError) {
+        negSum += err;
+    }
     
-    UR = Math.sqrt(sum * (1 / (hiterrors.length - 1))) * 10;
-
-    countp = 0; // сюда можно не смотреть, это чисто я считаю хитеррор
-    countm = 0;
-    divp = 0;
-    divm = 0;
-
-    for (i = 0; i < hiterrors.length; i++) {
-        if (hiterrors[i] < 0) {
-            divm += hiterrors[i]; countm++;
-        }
-        if (hiterrors[i] > 0) {
-            divp += hiterrors[i]; countp++;
-        }
+    for (const err of posHitError) {
+        posSum += err;
     }
+    
+    for (const err of allHitError) {
+        allSum += err;
+    }
+    
+    const negHitErrorAvg = negSum / negHitError.length;
+    const posHitErrorAvg = posSum / posHitError.length;
+    const allHitErrorAvg = allSum / allHitError.length;
+    
+    let variance = 0;
+    
+    for (const err of allHitError) {
+        variance += Math.pow(err - allHitErrorAvg, 2);
+    }
+    
+    variance /= allHitError.length;
+    
+    const unstableRate = Math.sqrt(variance) * 10;
 
-    divp /= countp;
-    divm /= countm;
-
-    console.log(`Error: ${divm} - ${divp}`)
-
+    console.log("negHitErrorAvg:", negHitErrorAvg);
+    console.log("posHitErrorAvg:", posHitErrorAvg);
+    console.log("unstableRate:", unstableRate);
 })
 
-console.log("OnlineScoreID: ", OnlineScoreID)
